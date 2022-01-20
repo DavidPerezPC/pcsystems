d9d1ed6b117e5348687ca2aef64bc40d423761fa
DavidPerezPC <davidperez@pcssistemas.com.mx>
2022-01-19 23:55:05 -0700

    Mejoras en paqutes Venuslift

--- addons/venuslift/models/sale_oder.py
+++ addons/venuslift/models/sale_oder.py
@@ -1,7 +1,9 @@
 # -*- coding: utf-8 -*-
 
+from email.policy import default
+from urllib.parse import quote_from_bytes
 from odoo import models, fields, api
-from datetime import date
+import datetime as dt
 
 class SaleOrder(models.Model):
     _inherit = ['sale.order']
@@ -26,8 +28,12 @@ class SaleOrder(models.Model):
                 help='Show if the Sales Order has been delivered',
                 compute='_get_delivered_status')
 
-    is_courtesy = fields.Boolean(default=False, string="Is courtesy?", help="Toggle if this sale is a courtesy")
-    
+    is_courtesy = fields.Boolean(default=False, 
+        string="Is courtesy?", 
+        help="Toggle if this sale is a courtesy")
+    ready_to_deliver = fields.Boolean(default=False, 
+            string="Ready to Deliver", 
+            help="Indicates that Sale Order can be delivered")        
     l10n_mx_edi_payment_method_id = fields.Many2one('l10n_mx_edi.payment.method',
         string="Payment Way",
         help="Indicates the way the invoice was/will be paid, where the options could be: "
@@ -35,6 +41,9 @@ class SaleOrder(models.Model):
         default=lambda self: self.env.ref('l10n_mx_edi.payment_method_otros', raise_if_not_found=False))
 
     product_uom_changed = fields.Boolean(compute='_get_delivered_status', store=False)
+    has_negative_stock = fields.Boolean(compute='_get_delivered_status',
+            string="Negative Stock", 
+            help="Indicates if a line item has no stock available")
 
     @api.depends('invoice_payment_state')
     def _get_invoice_payment_state(self):
@@ -48,21 +57,35 @@ class SaleOrder(models.Model):
             rec.invoice_payment_state = payment_state
             break
 
-    @api.depends('delivered_status', 'product_uom_changed')
-    def _get_delivered_status(self):
+    def action_confirm(self):
+        self._get_delivered_status()
+        if self.has_negative_stock:
+            return 
+        return super(SaleOrder, self).action_confirm()
 
+    @api.depends('delivered_status', 'product_uom_changed', 'has_negative_stock')
+    def _get_delivered_status(self):
+        sq_obj = self.env['stock.quant']
         for rec in self:
             status = 'nd'
             delivered = 0
             demand = 0 
             uom_changed = False
+            has_negative_stock = False
+            location_id = self.warehouse_id.lot_stock_id
             for line in rec.order_line:
                 if line.product_id.type == 'product':
                     delivered += line.qty_delivered
                     demand += line.product_uom_qty
                     if line.product_uom != line.product_id.uom_id \
                             and not uom_changed:
-                        uom_changed = True
+                        uom_changed = True   
+                    
+                    if rec.state in ['draft', 'sent'] and \
+                             not has_negative_stock:
+                        qty_available = sq_obj._get_available_quantity(line.product_id, location_id)          
+                        has_negative_stock = (qty_available < line.product_uom_qty)
+                
             if delivered == demand:
                 status = 'fd'
             elif delivered > 0 and delivered != demand:
@@ -72,25 +95,39 @@ class SaleOrder(models.Model):
 
             rec.delivered_status = status
             rec.product_uom_changed = uom_changed
+            rec.has_negative_stock = has_negative_stock
  
     #@api.multi()
     def _cancel_unconfirmed_so(self):
 
-        today = date.today()
-        domain = [('state', 'in', ['draft', 'sent'])]
-        for so in self.search(domain):
-            delta = today - so.create_date
-            if delta.days > 6:
-                so.action_cancel()
-
-
+        today = dt.datetime.now()
+        days = dt.timedelta(4)
+        date_to = dt.datetime.strftime(today - days, '%Y-%m-%d')
+        domain = [('state', 'in', ['draft', 'sent']),
+                    ('create_date', '<=', date_to),
+                    #('delivered_status', '=', 'nd'),
+                    #('invoice_payment_state', '=', 'not_paid')
+                    ]
+        sos = self.search(domain)
+        for so in sos:
+            so.action_cancel()
 
 # class SaleOrderLine(models.Model):
 #     _inherit = ['sale.order.line']
     
-#     @api.onchange('product_uom')
+
+#     has_negative_stock = fields.Boolean(store=True,
+#             string="Negative Stock", 
+#             help="Indicates if a line item has no stock available")
+
+#     @api.onchange('product_uom', 'product_uom_qty')
 #     def product_uom_change(self):
 #         super(SaleOrderLine, self).product_uom_change()
-#         self.order_id.product_uom_changed = True
-
+#         if self.product_id and self.product_id.type == 'product':
+#             sq_obj = self.env['stock.quant']
+#             location_id = self.order_id.warehouse_id.lot_stock_id
+#             qty_available = sq_obj._get_available_quantity(self.product_id, location_id)
+#             self.write({'has_negative_stock': (qty_available < self.product_uom_qty)})
+#             # if not self.order_id.has_negative_stock:
+#             #     self.order_id._get_delivered_status()
 
--- addons/venuslift/models/stock_picking.py
+++ addons/venuslift/models/stock_picking.py
@@ -2,12 +2,19 @@
 
 from odoo import models, fields, api
 
-class StockPikcing(models.Model):
+class StockPicking(models.Model):
     _inherit = ['stock.picking']
 
 
     payment_term_id = fields.Many2one(
         'account.payment.term', string='Payment Terms', related='sale_id.payment_term_id')
     invoice_payment_state = fields.Selection(related='sale_id.invoice_payment_state', string='Payment State', help='Payment state related to the Sale Order Invoice')
+    ready_to_deliver = fields.Boolean(related='sale_id.ready_to_deliver', string = 'To Deliver', help='Indicates if can be delivered')
 
-
+    @api.depends('state')
+    def _compute_show_validate(self):
+        super(StockPicking, self)._compute_show_validate()
+        for picking in self:
+            if picking.show_validate and not picking.ready_to_deliver:
+                picking.show_validate = False  
+    
