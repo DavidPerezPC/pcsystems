53a295105392df69ca01738aa6e05a5143fbfbd4
DavidPerezPC <davidperez@pcsistemas.com.mx>
2025-03-29 08:59:52 -0700

    [UPDATE] Dejar almacén en blanco en pedidos de ventas

--- addons/ferommis/models/sale_order.py
+++ addons/ferommis/models/sale_order.py
@@ -7,6 +7,11 @@ from odoo import fields, models, api, _
 class SaleOrder(models.Model):
     _inherit = "sale.order"
 
+    warehouse_id = fields.Many2one(
+        'stock.warehouse', string='Warehouse',
+        default=False, store=True, readonly=False, required=True,
+        check_company=True)
+    
     @api.onchange('partner_id')
     def onchange_partner_id(self):
         if self.partner_id:
