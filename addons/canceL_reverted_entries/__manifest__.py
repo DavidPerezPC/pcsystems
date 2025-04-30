b59817dca0cbc54f90585e6e41e195029a801cac
DavidPerezPC <davidperez@pcsistemas.com.mx>
2025-04-29 19:47:23 -0700

    [UPDATE] Módulo para revertir asientos conciliados

---
+++ addons/canceL_reverted_entries/__manifest__.py
@@ -0,0 +1,20 @@
+{
+    'name': 'Cancel Reverted Journal Entries',
+    'version': '1.0',
+    'summary': 'Allows cancellation of reverted journal entries in Odoo.',
+    'description': """
+        This module provides functionality to cancel journal entries that have been reverted, 
+        ensuring better control over accounting records.""",
+    'author': 'PC Systems',
+    'website': 'http://pcsystems.mx',
+    'category': 'Accounting',
+    'depends': ['account'],
+    'data': [
+        'security/ir.model.access.csv',
+        'wizard/account_move_inverted.xml',
+        # Add XML or CSV files here if needed, e.g., 'views/view_file.xml'
+    ],
+    'installable': True,
+    'application': False,
+    'license': 'LGPL-3',
+}
